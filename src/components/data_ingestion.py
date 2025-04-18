a427918796a1a52c5be6f21a1146a603a0f56d94
Varshitha-Gopalakrishna <varshithagopalakrishna@gmail.com>
2025-04-18 13:15:06 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,59 @@
-    
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        print("BEFORE LOGGING")  #C
+        logging.info("Entered the data ingestion method or component")
+        try:
+            logging.info("Trying to read the dataset")  #c
+            csv_path = r'E:\Projects\ML\notebook\data\StudentsPerformance.csv'  # Absolute path
+            logging.info(f"Using CSV file at path: {csv_path}")  # Log the file path
+
+            df = pd.read_csv(csv_path)
+
+            # Logging progress
+            logging.info('Read the dataset as dataframe')
+
+            # Create directories for saving data artifacts
+            logging.info("Creating directories for saving data artifacts...")
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            logging.info(f"Directory created or already exists: {os.path.dirname(self.ingestion_config.train_data_path)}")
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            logging.info("Train Test split initiated")
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=1)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info('Ingestion of data is completed')
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            logging.error(f"Error occurred: {str(e)}")  # Log the exception
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
